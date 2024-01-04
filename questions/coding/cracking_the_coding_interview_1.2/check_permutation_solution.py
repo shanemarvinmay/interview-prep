a02e92b61314be1041c4550759e9033b400f9d0a
shanemarvinmay <shanemarvinmay@gmail.com>
2024-01-04 12:58:49 -0800

    Improve cracking_the_coding_interview_1.2 by using only 1 Counter.

--- questions/coding/cracking_the_coding_interview_1.2/check_permutation_solution.py
+++ questions/coding/cracking_the_coding_interview_1.2/check_permutation_solution.py
@@ -2,14 +2,14 @@ from collections import Counter
 
 def check_permutation(s1, s2):
     s1_counter = Counter(s1)
-    s2_counter = Counter(s2)
+    
+    for ltr in s2:
+        if ltr not in s1_counter or s1_counter[ltr] == 0:
+            return False
+        s1_counter[ltr] -= 1
     
     for ltr in s1_counter:
-        if ltr not in s2_counter or s1_counter[ltr] != s2_counter[ltr]:
-            return False
-    
-    for ltr in s2_counter:
-        if ltr not in s1_counter:
+        if s1_counter[ltr]:
             return False
     
     return True
