df20627044f9cb3d264e0f562cefebb18519c656
shanemarvinmay <shanemarvinmay@gmail.com>
2024-01-09 20:13:06 -0800

    Add the rest of things learned.

--- study/python_crash_course_2e/what_i_learned.py
+++ study/python_crash_course_2e/what_i_learned.py
@@ -35,12 +35,49 @@ for value in a_dict.values():
     # print(value)
     pass
 
-# Taking an unknown numbers of arguments *ars
+# Taking an unknown numbers of arguments as a list (*ars)
 def print_car_features(car_name, *features):
     '''Print a car and it's features.'''
     print(car_name, end=': ')
     print(', '.join(features))
 # print_car_features('Batmobile', 'Jet engine', 'Bulletproof windows', 'Cup holders', 'Car seat for Robin')
+
+# Taking an unknown numbers of arguments as a dict (**kwargs)
+def create_new_job(title, **job_details):
+    job_details['job_title'] = title
+    for details, value in job_details.items():
+        print(details, value)
+    return job_details
+# create_new_job('ml engineer', money='a lot', location='hybrid')
+
+# After a successful try, you can use an else
+def try_except_else_example(throw_exception=False):
+    try:
+        print(0 / 0 if throw_exception else 1)
+    except ZeroDivisionError:
+        print('Caught you trying to divide by zero. Tsk..tsk... Rookie mistake')
+    else:
+        print('Nice job not dividing by zero.')
+
+# Test with unittest.
+import unittest
+import math
+def func_to_test(num):
+    try:
+        return round(num ** num  / math.log(num), 2)
+    except ValueError:
+        raise ValueError("Math domain error")
+class ExampleTestCase(unittest.TestCase):
+    """Example test case for func_to_test."""
+    def setUp(self) -> None:
+        # Do some stuff the will happen before the tests start.
+        return super().setUp()
+    def test_func_to_test_returns_correct_number(self):
+        """Test that func_to_test(2) returns 5.77"""
+        self.assertEqual(func_to_test(2), 5.77)
+    def test_func_to_test_throws_exception(self):
+        with self.assertRaises(ValueError):
+         func_to_test(0)
 
 if __name__ == '__main__':
     print(some_list == a_copy == another_copy)
@@ -58,3 +95,5 @@ if __name__ == '__main__':
     print(f'another_copy:{another_copy}')
     print(f'another_deep_copy:{another_deep_copy}')
     print(f'shallow_copy:{shallow_copy}')
+    print(func_to_test(2))
+    unittest.main()
