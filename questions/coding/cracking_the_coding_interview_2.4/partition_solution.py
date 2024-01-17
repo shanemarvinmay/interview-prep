1fca98f2e4889b53ad058ddeab95a73bd363e308
shanemarvinmay <shanemarvinmay@gmail.com>
2024-01-17 06:57:05 -0800

    Complete cracking_the_coding_interview_2.4.

---
+++ questions/coding/cracking_the_coding_interview_2.4/partition_solution.py
@@ -0,0 +1,40 @@
+def partition_linked_list(head, partition):
+    if head is None:
+        return None, None
+    
+    equal_greater = head
+    less = None
+    while equal_greater:
+        # Fiding the first/next node that is equal or greater than the partition.
+        while equal_greater and equal_greater.value < partition:
+            equal_greater = equal_greater.next
+        if equal_greater and less is None:
+            less = equal_greater.next
+        # Fiding the next node after equal_greater that is less than the partition.
+        while less and less.value >= partition:
+            less = less.next
+        
+        if less is None or equal_greater is None:
+            break
+        # Swap
+        temp = equal_greater.value
+        equal_greater.value = less.value
+        less.value = temp
+        
+        equal_greater = equal_greater.next
+    
+    # All nodes < partition
+    if equal_greater is None and less is None:
+        return head, None
+    
+    # Finding the head of the right.
+    itr = head
+    while itr.next and itr.next.value < partition:
+        itr = itr.next
+        
+    equal_greater_head = itr.next
+    itr.next = None
+
+    return head, equal_greater_head
+
+        
---
+++ questions/coding/cracking_the_coding_interview_2.4/partition_test.py
@@ -0,0 +1,81 @@
+from partition_solution import partition_linked_list
+
+# Single and doubly linked list
+class Node:
+    '''Node that can be used for single or double linked list.'''
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+def list_to_linked_list(l):
+    if len(l) < 0:
+        return
+    
+    head = Node(l[0])
+    itr = head
+    for i in range(1, len(l)):
+        itr.next = Node(l[i])
+        itr = itr.next
+    
+    return head
+
+def assert_partitions(less, equal_greater, partition):
+    # print('less')
+    while less:
+        # print(less.value)
+        assert less.value < partition
+        less = less.next
+    # print('equal_greater')
+    while equal_greater:
+        # print(equal_greater.value)
+        assert equal_greater.value >= partition
+        equal_greater = equal_greater.next
+
+# Edge cases 
+
+# all less than partition
+def test_partition_linked_list_all_values_less():
+    ll = list_to_linked_list([0, 1, 2])
+    partition = 3
+
+    less, equal_greater = partition_linked_list(ll, partition)
+
+    assert_partitions(less, equal_greater, partition)
+
+# all greater than or equal to partition
+def test_partition_linked_list_all_values_equal_greater():
+    ll = list_to_linked_list([0, 1, 2])
+    partition = 0
+
+    less, equal_greater = partition_linked_list(ll, partition)
+    
+    assert_partitions(less, equal_greater, partition)
+
+# normal case
+def test_partition_linked_list_normal_case():
+    ll = list_to_linked_list([2, 1, 0])
+    partition = 1
+
+    less, equal_greater = partition_linked_list(ll, partition)
+    print('less list')
+    itr = less
+    while itr:
+        print(itr.value, end='->')
+        itr = itr.next
+    print()
+    print('equal_greater')
+    itr = equal_greater
+    while itr:
+        print(itr.value, end='->')
+        itr = itr.next
+
+    assert_partitions(less, equal_greater, partition)
+
+# empty list
+def test_partition_linked_list_empty_list():
+    less, equal_greater = partition_linked_list(None, 0)
+
+    assert less is None
+    assert equal_greater is None
+
