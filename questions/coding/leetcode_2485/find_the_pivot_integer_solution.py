2727eb1b21bfa576a2e56d28d955567671f9ad54
shanemarvinmay <shanemarvinmay@gmail.com>
2024-01-18 06:21:23 -0800

    Fix leetcode_2485.

--- questions/coding/leetcode_2485/find_the_pivot_integer_solution.py
+++ questions/coding/leetcode_2485/find_the_pivot_integer_solution.py
@@ -3,19 +3,18 @@ class Solution:
         high = higher_sum = n
         low = lower_sum = 1
         
-        while low < high and lower_sum != higher_sum or lower_sum == 0:
+        while low != high:
             print(f'lower_sum:{lower_sum}\tlow:{low}\thigher_sum:{higher_sum}\thigh:{high}')
-            while lower_sum < higher_sum:
+            if lower_sum < higher_sum:
                 low += 1
                 lower_sum += low
                 print(f'lower_sum:{lower_sum}\tlow:{low}\thigher_sum:{higher_sum}\thigh:{high}')
-            while lower_sum > higher_sum:
+            else:
                 high -= 1
                 higher_sum += high
                 print(f'lower_sum:{lower_sum}\tlow:{low}\thigher_sum:{higher_sum}\thigh:{high}')
 
         print(f'lower_sum:{lower_sum}\tlow:{low}\thigher_sum:{higher_sum}\thigh:{high}')
-        middle = (high + low) / 2
-        if lower_sum == higher_sum and middle == round(middle):
-            return middle
+        if lower_sum == higher_sum:
+            return high # could also be low
         return -1
--- questions/coding/leetcode_2485/find_the_pivot_integer_test.py
+++ questions/coding/leetcode_2485/find_the_pivot_integer_test.py
@@ -14,3 +14,11 @@ def test_pivot_integer_solution(pivot_integer):
 # There is no solution
 def test_pivot_integer_no_solution(pivot_integer):
     assert pivot_integer(4) == -1
+
+''' The original solution found when lower_sum == higher_sum, then took the
+average of low and high (if it was a round number). This worked for the above
+examples but failed for 15. So this is to ensure the new solution works.
+(It does. So don't break it!)
+'''
+def test_pivot_integer_no_mid_solution(pivot_integer):
+    assert pivot_integer(15) == -1
