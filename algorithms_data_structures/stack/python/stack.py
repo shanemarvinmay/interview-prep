e65c567d347ca94dc8f6ff4e7740706f725a9583
shanemarvinmay <shanemarvinmay@gmail.com>
2024-01-25 09:45:56 -0800

    Implement Stack in python.

---
+++ algorithms_data_structures/stack/python/stack.py
@@ -0,0 +1,38 @@
+class Stack:
+    '''This is the Stack classed implemented in python with nodes.
+
+    Nodes are being used here since the list (vector) implementation is already done in python.'''
+
+    class Node:
+        def __init__(self, value):
+            self.value = value
+            self.next = None
+
+    def __init__(self):
+        self.top = None
+    
+    def push(self, value):
+        # Stack is empty.
+        if self.top is None:
+            self.top = self.Node(value)
+        else:
+            node = self.Node(value)
+            node.next = self.top
+            self.top = node
+
+    def pop(self):
+        # Stack is empty
+        if self.top is None:
+            return None
+        value = self.top.value
+        self.top = self.top.next
+        return value
+
+    def peek(self):
+        # Stack is empty.
+        if self.top is None:
+            return None
+        return self.top.value
+
+    def is_empty(self):
+        return self.top is None
---
+++ algorithms_data_structures/stack/python/stack_test.py
@@ -0,0 +1,32 @@
+from stack import Stack
+
+import pytest
+
+def test_push_pop():
+    stack = Stack()
+
+    for value in range(5):
+        stack.push(value)
+
+    for expected in range(4, -1, -1):
+        assert stack.pop() == expected
+    
+    assert stack.pop() is None
+
+def test_peek():
+    stack = Stack()
+
+    assert stack.peek() is None
+
+    stack.push(0)
+
+    assert stack.peek() == 0 and not stack.is_empty()
+
+def test_is_empty():
+    stack = Stack()
+
+    assert stack.is_empty()
+
+    stack.push(0)
+
+    assert not stack.is_empty()
