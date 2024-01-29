82940bdd858223cc485d07940c017f68a20034e7
shanemarvinmay <shanemarvinmay@gmail.com>
2024-01-29 08:29:41 -0800

    Update util to take in title and print filename.

--- questions/coding/util/util.py
+++ questions/coding/util/util.py
@@ -2,3 +2,6 @@ def leetcode_title_to_file_name(title):
     title = title.lower()
     title = title.replace(' ', '_')
     return title
+
+title = input('Enter leetcode title:')
+print('file_name:', leetcode_title_to_file_name(title))
