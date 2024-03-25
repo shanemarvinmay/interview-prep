bab779dce1d75c67a61de58517249b6f8000c774
shanemarvinmay <shanemarvinmay@gmail.com>
2024-03-25 07:06:12 -0700

    Add note for improvement.

--- questions/coding/leetcode_1356/sort_integers_by_the_number_of_1_bits_solution.py
+++ questions/coding/leetcode_1356/sort_integers_by_the_number_of_1_bits_solution.py
@@ -1,5 +1,8 @@
 class Solution:
     def sortByBits(self, arr: list[int]) -> list[int]:
+        # TODO implement one or both of the following solutions:
+        # https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/solutions/2715364/simple-java-solution-easy-to-understand/
+        # https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/solutions/4224485/96-58-built-in-functions-brian-kerninghan-s-algorithm-explained-intuition/
         arr.sort()
 
         # Get binary 1s count and zip them in the list
@@ -12,6 +15,6 @@ class Solution:
 
         # Sort based on 1s
         arr = sorted(arr, key=lambda x: x[1])
-        
+
         # return numbers
         return [num for num, _ in arr]
