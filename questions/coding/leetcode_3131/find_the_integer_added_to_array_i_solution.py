00e31bb4bccc371f3b37b4a1a0b477acbbb044de
shanemarvinmay <shanemarvinmay@gmail.com>
2024-06-04 12:26:49 -0700

    Improve leetcode_3131.

--- questions/coding/leetcode_3131/find_the_integer_added_to_array_i_solution.py
+++ questions/coding/leetcode_3131/find_the_integer_added_to_array_i_solution.py
@@ -1,7 +1,9 @@
 from typing import List
 class Solution:
     def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:
-        sum_1 = sum(nums1)
-        sum_2 = sum(nums2)
-        diff = sum_2 - sum_1
-        return diff // len(nums1)
+        return min(nums2) - min(nums1)
+        # Attempt #0
+        # sum_1 = sum(nums1)
+        # sum_2 = sum(nums2)
+        # diff = sum_2 - sum_1
+        # return diff // len(nums1)
