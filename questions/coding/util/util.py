c6b458604e7fd55d00cfc3e1b48c86929751cf28
shanemarvinmay <shanemarvinmay@gmail.com>
2024-07-05 11:02:43 -0700

    Set up utils.

--- questions/coding/util/util.py
+++ questions/coding/util/util.py
@@ -1,7 +1,9 @@
-def leetcode_title_to_file_name(title):
-    title = title.lower()
-    title = title.replace(' ', '_')
-    return title
+def assert_binary_trees_are_equal(t1, t2):
+    if t1 is None or t2 is None:
+        assert t1 is None and t2 is None
+        return
+    
+    assert t1.val == t2.val
 
-title = input('Enter leetcode title:')
-print('file_name:', leetcode_title_to_file_name(title))
+    assert_binary_trees_are_equal(t1.left, t2.left)
+    assert_binary_trees_are_equal(t1.right, t2.right)
