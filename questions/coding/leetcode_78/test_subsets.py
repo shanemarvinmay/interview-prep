767f4fe2bf47b97ff888cc76a8a25c3afd4ea4ae
shanemarvinmay <shanemarvinmay@gmail.com>
2024-07-13 12:40:01 -0700

    Apply formatting fix.

--- questions/coding/leetcode_78/test_subsets.py
+++ questions/coding/leetcode_78/test_subsets.py
@@ -5,18 +5,7 @@ import pytest
 @pytest.fixture()
 def sol():
     return Solution()
-"""
-Example 1:
-Input: nums = 
-Output: 
-Example 2:
-Input: nums = [0]
-Output: [[],[0]]
- 
-Constraints:
-1 <= nums.length <= 10
--10 <= nums[i] <= 10
-All the numbers of nums are unique."""
+
 @pytest.mark.parametrize('nums, expected', [
     ([1,2,3], [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]),
     ([0], [[], [0]]),
