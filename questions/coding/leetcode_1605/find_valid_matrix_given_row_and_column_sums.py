1aa1f4c9babb49e17e609bd6228d4d0aab866957
shanemarvinmay <shanemarvinmay@gmail.com>
2024-07-20 18:53:07 -0700

    Complete leetcode_1605.

--- questions/coding/leetcode_1605/find_valid_matrix_given_row_and_column_sums.py
+++ questions/coding/leetcode_1605/find_valid_matrix_given_row_and_column_sums.py
@@ -1,19 +1,38 @@
 from typing import List
 
 class Solution:
-    def __init__(self):
-        self.matrix = []
-        self.row_sums = []
-        self.col_sums = []
     def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
-        self.row_sums = rowSum
-        self.col_sums = colSum
-        self.init_matrix()
+        rows = len(rowSum)
+        cols = len(colSum)
+        cur_row = 0
+        cur_col = 0
         
-    def init_matrix(self):
-        n = len(self.row_sums)
-        for row in self.row_sums:
-            self.matrix.append([])
-            for col in self.col_sums:
-                num = (row + col) // n
-                self.matrix[-1].append(num)
+        # Init matrix
+        matrix = []
+        for i in range(rows):
+            row = []
+            for j in range(cols):
+                row.append(0)
+            matrix.append(row)
+
+        while cur_row < rows or cur_col < cols:
+            if cur_row >= rows:
+                matrix[-1][cur_col] = colSum[cur_col]
+                cur_col += 1
+                continue
+            if cur_col >= cols:
+                matrix[cur_row][-1] = rowSum[cur_row]
+                cur_row += 1
+                continue
+
+            value = min(rowSum[cur_row], colSum[cur_col])
+            rowSum[cur_row] -= value
+            colSum[cur_col] -= value
+            matrix[cur_row][cur_col] = value
+
+            if rowSum[cur_row] == 0:
+                cur_row += 1
+            if colSum[cur_col] == 0:
+                cur_col += 1
+        
+        return matrix
--- questions/coding/leetcode_1605/test_find_valid_matrix_given_row_and_column_sums.py
+++ questions/coding/leetcode_1605/test_find_valid_matrix_given_row_and_column_sums.py
@@ -21,23 +21,3 @@ Constraints:
 0 <= rowSum[i], colSum[i] <= 108
 sum(rowSum) == sum(colSum)
 '''
-from questions.coding.leetcode_1605.find_valid_matrix_given_row_and_column_sums import Solution
-
-import pytest
-
-@pytest.fixture()
-def sol():
-    return Solution()
-
-def test_init_matrix(sol):
-    expected = [
-        [3, 5],
-        [6, 7]
-    ]
-    sol.row_sums = [3, 8]
-    sol.col_sums = [4, 7]
-    
-    sol.init_matrix()
-    got = sol.matrix
-
-    assert got == expected
