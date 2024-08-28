a21bda7d4271313f094e0d36d3ade49974be5b91
shanemarvinmay <shanemarvinmay@gmail.com>
2024-08-28 12:20:06 -0700

    Apply formatting fix.

--- questions/coding/leetcode_2225/find_players_with_zero_or_one_losses.py
+++ questions/coding/leetcode_2225/find_players_with_zero_or_one_losses.py
@@ -17,26 +17,3 @@ class Solution:
         winners = sorted(list(winners))
         single_losers = sorted(list(single_losers))
         return [winners, single_losers]
-
-'''
-
-
-Input: matches = 
-Explanation:
-Players 1, 2, and 10 have not lost any matches.
-Players 4, 5, 7, and 8 each have lost one match.
-Players 3, 6, and 9 each have lost two matches.
-Thus, answer[0] = [1,2,10] and answer[1] = [4,5,7,8].
-Example 2:
-Input: matches = 
-Explanation:
-Players 1, 2, 5, and 6 have not lost any matches.
-Players 3 and 4 each have lost two matches.
-Thus, answer[0] = [1,2,5,6] and answer[1] = [].
-Constraints:
-1 <= matches.length <= 105
-matches[i].length == 2
-1 <= winneri, loseri <= 105
-winneri != loseri
-All matches[i] are unique.
-'''
