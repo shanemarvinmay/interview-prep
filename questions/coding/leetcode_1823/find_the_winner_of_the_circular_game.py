7281aac77bb993f0f2e2c82928ff6ba7fbf3fd02
shanemarvinmay <shanemarvinmay@gmail.com>
2024-08-31 15:39:42 -0700

    Complete leetcode_1823.

--- questions/coding/leetcode_1823/find_the_winner_of_the_circular_game.py
+++ questions/coding/leetcode_1823/find_the_winner_of_the_circular_game.py
@@ -1,22 +1,16 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        friends = [1] * n
+        winners = set()
+        for i in range(1, n+1):
+            winners.add(i)
+        i = 1
+        skips = k
+        while len(winners) > 1:
+            if i in winners:
+                skips -= 1
+            if skips == 0:
+                winners.remove(i)
+                skips = k
+            i = 1 if i+1 > n else i+1
 
-        i = 0
-        while friends.count(1) != 1:
-            if friends.count(1) == 0:
-                friends = [1] * n
-            i = self.get_next_idx(friends, i, k-1)
-            friends[i] = 0
-
-        return friends.index(1) + 1
-    def get_next_idx(self, friends, i, k):
-        n = len(friends)
-        while friends[i] == 0:
-            i = i+1 if i+1 < n else 0
-        while k:
-            i = i+1 if i+1 < n else 0
-            if friends[i] == 0:
-                continue
-            k -= 1
-        return i
+        return winners.pop()
