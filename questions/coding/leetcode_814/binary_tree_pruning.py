fc3a3c8c623f023d0b09230fbb58ab28c11166f5
shanemarvinmay <shanemarvinmay@gmail.com>
2024-09-13 06:14:08 -0700

    Complete leetcode_814.

---
+++ questions/coding/leetcode_814/binary_tree_pruning.py
@@ -0,0 +1,16 @@
+from typing import Optional
+# tested in leetcode
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None: return
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        if root.val == 0 and root.left is None and root.right is None:
+            del root
+        else:
+            return root
