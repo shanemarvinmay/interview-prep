c511ae296a6f4b659b8085c97c083ff6494846bc
shanemarvinmay <shanemarvinmay@gmail.com>
2024-11-03 18:15:24 -0800

    Complete insertion_sort.

---
+++ algorithms_data_structures/sorting/insertion_sort.py
@@ -0,0 +1,8 @@
+def insertion_sort(ar):
+    n = len(ar)
+    for i in range(1, n):
+        idx = i
+        for j in range(i-1, -1, -1):
+            if ar[j] > ar[idx]:
+                ar[idx], ar[j] = ar[j], ar[idx]
+                idx = j
--- algorithms_data_structures/sorting/sorting_test.py
+++ algorithms_data_structures/sorting/sorting_test.py
@@ -1,6 +1,6 @@
 from algorithms_data_structures.sorting.bubble_sort import bubble_sort
 from algorithms_data_structures.sorting.selection_sort import selection_sort
-
+from algorithms_data_structures.sorting.insertion_sort import insertion_sort
 
 def test_bubble_sort():
     ar = [64, 34, 25, 12, 22, 11, 90, 5]
@@ -13,5 +13,12 @@ def test_selection_sort():
     ar = [64, 34, 25, 12, 22, 11, 90, 5]
 
     selection_sort(ar)
+
+    assert ar == [5, 11, 12, 22, 25, 34, 64, 90]
+
+def test_insertion_sort():
+    ar = [64, 34, 25, 12, 22, 11, 90, 5]
+
+    insertion_sort(ar)
 
     assert ar == [5, 11, 12, 22, 25, 34, 64, 90]
