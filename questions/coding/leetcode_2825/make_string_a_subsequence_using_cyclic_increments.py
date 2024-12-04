7d4e40048c49687653d81bd824e0670f39831b63
shanemarvinmay <shanemarvinmay@gmail.com>
2024-12-04 10:48:30 -0800

    Complete leetcode_2825.

--- questions/coding/leetcode_2825/make_string_a_subsequence_using_cyclic_increments.py
+++ questions/coding/leetcode_2825/make_string_a_subsequence_using_cyclic_increments.py
@@ -1,17 +1,17 @@
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
         if len(str2) > len(str1): return False
-        # Making map of str2
-        str2_map = dict()
-        for ltr in str2:
-            num = ord(ltr)
-            if num not in str2_map:
-                str2_map[num] = 1
-            str2_map[num] += 1
-        # Checking if we can make every letter in str2 from str1
-        for ltr in str1:
-            num = ord(ltr)
-            if num in str2_map:
-                str2_map[num] -= 1
-            
+        i = j = 0
+        while j < len(str2):
+            num2 = ord(str2[j])
+            j += 1
+            changed = False
+            while i < len(str1) and not changed:
+                num1 = ord(str1[i])
+                i += 1
+                if num1 != num2:
+                    num1 = num1 + 1 if num1 < 122 else 97
+                if num1 == num2:
+                    changed = True
+            if not changed: return False
         return True
--- questions/coding/leetcode_2825/test_make_string_a_subsequence_using_cyclic_increments.py
+++ questions/coding/leetcode_2825/test_make_string_a_subsequence_using_cyclic_increments.py
@@ -6,6 +6,8 @@ import pytest
     ('abc', 'ad', True),
     ('zc', 'ad', True),
     ('ab', 'd', False),
+    ('a', 'ab', False),
+    ("eao", "ofa", False),
 ])
 def test_canMakeSubsequence(s1, s2, expected):
     sol = Solution()
