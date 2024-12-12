18195227d1f82541f11ab4e397a6a77da9681d85
shanemarvinmay <shanemarvinmay@gmail.com>
2024-12-12 14:21:43 -0800

    Improve leetcode_261 slightly.

--- questions/coding/leetcode_261/graph_valid_tree.py
+++ questions/coding/leetcode_261/graph_valid_tree.py
@@ -14,26 +14,21 @@ class Solution:
         VISITED = 2
         states = [UNVISITED] * n
 
-        def cycle_detected(v, history = None):
+        def cycle_detected(v, prev=-1):
             if states[v] == VISITED:
                 return False
             elif states[v] == VISITING:
                 return True
-            if history is None:
-                history = []
             
             states[v] = VISITING
-            history.append(v)
-
 
             for neighbor in hm[v]:
                 # Make sure we don't go back and forth
                 # on the same edge.
-                if len(history) > 1 and neighbor == history[-2]: continue
-                if cycle_detected(neighbor, history): return True
+                if neighbor == prev: continue
+                if cycle_detected(neighbor, prev=v): return True
             
             states[v] = VISITED
-            history.pop()
             return False
         
         if cycle_detected(0):
